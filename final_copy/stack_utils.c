b811a606f057e4c2aa196d5e200dd0a23f6cce23
Dvmb455B1tch <ritsoniaina42@gmail.com>
2025-05-18 19:37:16 +0300

    eo amzay fa lele

--- final_copy/stack_utils.c
+++ final_copy/stack_utils.c
@@ -1,4 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riramana <riramana@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 11:18:51 by riramana          #+#    #+#             */
+/*   Updated: 2025/05/18 11:18:53 by riramana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
+
+int	is_number(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (is_sign(str[i]) && str[i + 1] != '\0')
+		i++;
+	while (str[i] && is_digit(str[i]))
+		i++;
+	if (str[i] != '\0')
+		return (0);
+	return (1);
+}
 
 void	free_stack(t_node **stack)
 {
