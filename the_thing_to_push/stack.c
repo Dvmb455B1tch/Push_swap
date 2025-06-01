f4ca4078400fe69749d62358c67add2bf5160ab4
Ritsoniaina Arovony Ramanarivosoa <riramana@c1r12s10.42antananarivo.mg>
2025-06-01 11:38:30 +0300

    Rearranging

---
+++ the_thing_to_push/stack.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   stack.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riramana <riramana@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/14 10:14:30 by riramana          #+#    #+#             */
+/*   Updated: 2025/05/14 10:14:33 by riramana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_node	*new_node(int value)
+{
+	t_node	*node;
+
+	node = malloc(sizeof(t_node));
+	if (!node)
+		return (NULL);
+	node->value = value;
+	node->index = 0;
+	node->position = -1;
+	node->target_pos = -1;
+	node->cost_a = -1;
+	node->cost_b = -1;
+	node->next = NULL;
+	return (node);
+}
+
+void	add_node_back(t_node **stack, t_node *new)
+{
+	t_node	*last;
+
+	if (!new)
+		return ;
+	if (!*stack)
+	{
+		*stack = new;
+		return ;
+	}
+	last = get_last_node(*stack);
+	last->next = new;
+}
+
+t_node	*get_last_node(t_node *stack)
+{
+	if (!stack)
+		return (NULL);
+	while (stack->next)
+		stack = stack->next;
+	return (stack);
+}
+
+t_node	*get_second_last_node(t_node *stack)
+{
+	if (!stack || !stack->next)
+		return (NULL);
+	while (stack->next && stack->next->next)
+		stack = stack->next;
+	return (stack);
+}
+
+int	stack_size(t_node *stack)
+{
+	int	size;
+
+	size = 0;
+	while (stack)
+	{
+		size++;
+		stack = stack->next;
+	}
+	return (size);
+}
---
+++ the_thing_to_push/validation.c
@@ -0,0 +1,127 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   validation.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: riramana <riramana@student.42antananarivo  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/13 09:20:51 by riramana          #+#    #+#             */
+/*   Updated: 2025/05/13 13:54:55 by riramana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	is_zero(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (is_sign(str[i]))
+		i++;
+	while (str[i] && str[i] == '0')
+		i++;
+	if (str[i] != '\0')
+		return (0);
+	return (1);
+}
+
+int	has_duplicates(char **av)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (av[i])
+	{
+		j = 1;
+		while (av[j])
+		{
+			if (i != j && ft_atoi(av[i]) == ft_atoi(av[j]))
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	check_values_in_str(char *str, long *values, int *count)
+{
+	int		j;
+	int		k;
+	char	buffer[12];
+	long	value;
+
+	j = 0;
+	while (str[j])
+	{
+		while (str[j] == ' ')
+			j++;
+		if (str[j] == '\0')
+			break ;
+		k = 0;
+		while (str[j] && str[j] != ' ' && k < 11)
+			buffer[k++] = str[j++];
+		buffer[k] = '\0';
+		if (!is_number(buffer))
+			return (0);
+		value = ft_atoi(buffer);
+		if (value > 2147483647 || value < -2147483648)
+			return (0);
+		values[(*count)++] = value;
+	}
+	return (1);
+}
+
+static int	check_duplicates_in_values(long *values, int count, int *zero_count)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	*zero_count = 0;
+	while (i < count)
+	{
+		if (values[i] == 0)
+			(*zero_count)++;
+		j = i + 1;
+		while (j < count)
+		{
+			if (values[i] == values[j])
+				return (1);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
+
+int	check_input(int ac, char **av)
+{
+	int		i;
+	int		j;
+	int		count;
+	long	values[1024];
+
+	if (ac < 2)
+		return (0);
+	i = 0;
+	count = 0;
+	while (++i < ac)
+	{
+		if (av[i][0] == '\0')
+			return (0);
+		j = 0;
+		while (av[i][j] == ' ')
+			j++;
+		if (av[i][j] == '\0')
+			return (0);
+		if (!check_values_in_str(av[i], values, &count))
+			return (0);
+	}
+	if (!count || check_duplicates_in_values(values, count, &i)
+		|| has_duplicates(av))
+		return (0);
+	return (1);
+}
