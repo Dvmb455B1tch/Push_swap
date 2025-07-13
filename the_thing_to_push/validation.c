e272fafe3a7864b9ff2b679422d3851a87932b11
Dvmb455B1tch <ritsoniaina42@gmail.com>
2025-07-13 20:06:53 +0300

    the_thing_to_push ..../ update : i fixed it , and at the cost of a few insults

--- the_thing_to_push/validation.c
+++ the_thing_to_push/validation.c
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*   validation.c                                       :+:      :+:    :+:   */
+
 #include "push_swap.h"
 
 int	has_duplicates(char **av)
@@ -60,43 +62,23 @@ static int	check_values_in_str(char *str, long *values, int *count)
 	return (1);
 }
 
-static int	check_duplicates_in_values(long *values, int count, int *zero_count)
+static int	validate_and_count_args(int ac, char **av)
 {
 	int	i;
 	int	j;
+	int	max_values;
 
-	i = 0;
-	*zero_count = 0;
-	while (i < count)
-	{
-		if (values[i] == 0)
-			(*zero_count)++;
-		j = i + 1;
-		while (j < count)
-		{
-			if (values[i] == values[j])
-				return (1);
-			j++;
-		}
-		i++;
-	}
-	return (0);
-}
-
-int	check_input(int ac, char **av)
-{
-	int		i;
-	int		j;
-	int		count;
-	long	*values;
-	int		max_values;
-
-	if (ac < 2)
-		return (0);
 	max_values = 0;
 	i = 0;
 	while (++i < ac)
 	{
+		if (av[i][0] == '\0')
+			return (0);
+		j = 0;
+		while (av[i][j] == ' ')
+			j++;
+		if (av[i][j] == '\0')
+			return (0);
 		j = 0;
 		while (av[i][j])
 		{
@@ -106,38 +88,52 @@ int	check_input(int ac, char **av)
 		}
 		max_values++;
 	}
-	values = malloc(sizeof(long) * max_values);
-	if (!values)
-		return (0);
+	return (max_values);
+}
+
+static int	process_arguments(int ac, char **av, long *values)
+{
+	int	i;
+	int	count;
+	int	j;
+
 	i = 0;
 	count = 0;
 	while (++i < ac)
 	{
-		if (av[i][0] == '\0')
-		{
-			free(values);
-			return (0);
-		}
-		j = 0;
-		while (av[i][j] == ' ')
-			j++;
-		if (av[i][j] == '\0')
-		{
-			free(values);
-			return (0);
-		}
 		if (!check_values_in_str(av[i], values, &count))
-		{
-			free(values);
 			return (0);
-		}
 	}
-	if (!count || check_duplicates_in_values(values, count, &i)
-		|| has_duplicates(av))
+	i = 0;
+	while (i < count)
 	{
-		free(values);
-		return (0);
+		j = i + 1;
+		while (j < count)
+		{
+			if (values[i] == values[j])
+				return (0);
+			j++;
+		}
+		i++;
 	}
+	return (!has_duplicates(av));
+}
+
+int	check_input(int ac, char **av)
+{
+	long	*values;
+	int		max_values;
+	int		result;
+
+	if (ac < 2)
+		return (0);
+	max_values = validate_and_count_args(ac, av);
+	if (max_values == 0)
+		return (0);
+	values = malloc(sizeof(long) * max_values);
+	if (!values)
+		return (0);
+	result = process_arguments(ac, av, values);
 	free(values);
-	return (1);
+	return (result);
 }
